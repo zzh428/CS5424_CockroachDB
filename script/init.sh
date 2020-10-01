80dfa4649c77f9e142d637621a6d29624af6c42d
rayzui <1430527996@qq.com>
2020-10-01 16:47:00 +0800

    init error fix

--- script/init.sh
+++ script/init.sh
@@ -1,3 +1,8 @@
-DIR="/Users/zzh/Desktop/cockroach-data"
+if [ ! -n "$1" ] ;then
+    DIR=$(pwd)/cockroach-data
+else
+    DIR=$1
+fi
+echo "Data dir:" $DIR
 cockroach start-single-node --insecure --listen-addr=localhost:26257 --http-addr=localhost:8080 --store $DIR --background
 cockroach sql --insecure --host=localhost:26257 < ../src/init.sql
